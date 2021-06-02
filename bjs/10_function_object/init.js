5c417f7851945c64a0c55140fdef45e8d313c447
Andbaik <andbaik@mail.ru>
2021-06-02 22:27:35 +0300

    add button

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -2,6 +2,10 @@
 window.onload = function()
 {
     const initPerson = personGenerator.getPerson();
+    document.getElementById('surnameOutput').innerText = initPerson.surnameOutput;
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    document.getElementById('lastNameOutput').innerText = initPerson.lastNameOutput;
+    document.getElementById('genderOutput').innerText = initPerson.genderOutput;
+    document.getElementById('birthYearOutput').innerText = initPerson.birthYearOutput;
 };
 
